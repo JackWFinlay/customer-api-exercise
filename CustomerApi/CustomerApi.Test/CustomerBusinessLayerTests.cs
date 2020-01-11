171239921c7bb567ca005bd0bf9cbb30f2f235c6
Jack Finlay <jack@jackfinlay.com>
2020-01-12 12:33:35 +1300

    Add test project.

---
+++ CustomerApi/CustomerApi.Test/CustomerBusinessLayerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomerApi.Test
+{
+    public class CustomerBusinessLayerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
