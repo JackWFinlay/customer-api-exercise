cf248584a224bd74248931fd9b14ddda57811343
Jack Finlay <jack@jackfinlay.com>
2020-01-12 22:24:47 +1300

    Remove commented out code.

--- CustomerApi/CustomerApi.Api/Startup.cs
+++ CustomerApi/CustomerApi.Api/Startup.cs
@@ -31,14 +31,9 @@ namespace CustomerApi.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddLogging(l => l.AddConsole()
-            //                          .AddDebug()
-            //);
-
             services.AddDbContext<CustomerContext>(c => c.UseInMemoryDatabase("CustomerDB"));
             services.AddScoped<ICustomerStorageProvider, InMemoryCustomerStorageProvider>();
 
-            //services.AddScoped<ILogger>();
             services.AddScoped<CustomerMapper>();
             services.AddScoped<CustomerBusinessLayer>();
 
